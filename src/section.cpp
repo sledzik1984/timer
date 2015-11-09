3b25290160b2ed8ee3623e0719c68d9d2d1f7100
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-11-09 01:40:36 -0500

    Section: rewrite

--- src/section.cpp
+++ src/section.cpp
@@ -1,2 +1,32 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "section.hpp"
+#include <stdexcept>
+
+////////////////////////////////////////////////////////////////////////////////
+Section::Section(QString name, Section::Duration duration)
+{
+    set_name(std::move(name));
+    set_duration(std::move(duration));
+    reset();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::start()
+{
+    if(!is_started()) _started = Clock::now();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::end()
+{
+    if(is_started() && !is_ended()) _ended = Clock::now();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Section::Duration Section::overage() const
+{
+    return is_started() ? std::chrono::duration_cast<Duration>(
+                              (started() + duration()) - (is_ended() ? ended() : Clock::now())
+                          )
+                        : Duration(0);
+}
--- src/section.hpp
+++ src/section.hpp
@@ -3,22 +3,28 @@
 #define SECTION_HPP
 
 #include <QString>
-#include <QTime>
 
+#include <chrono>
 #include <utility>
 
 ////////////////////////////////////////////////////////////////////////////////
 class Section
 {
 public:
-    ////////////////////
-    Section() = default;
+    using Duration = std::chrono::seconds;
 
-    Section(QString name, QTime time)
-    {
-        set_name(std::move(name));
-        set_time(std::move(time));
-    }
+    using Clock = std::chrono::system_clock;
+    using Timepoint = std::chrono::time_point<Clock>;
+
+    ////////////////////
+    Section() { reset(); }
+
+    template<typename Rep, typename Period>
+    Section(QString name, const std::chrono::duration<Rep, Period>& duration) :
+        Section(std::move(name), std::chrono::duration_cast<Duration>(duration))
+    { }
+
+    Section(QString name, Duration duration);
 
     ////////////////////
     Section(const Section&) = default;
@@ -31,13 +37,42 @@ public:
     void set_name(QString name) { _name = std::move(name); }
     const QString& name() const noexcept { return _name; }
 
+    template<typename Rep, typename Period>
+    void set_duration(const std::chrono::duration<Rep, Period>& duration)
+    {
+        set_duration(std::chrono::duration_cast<Duration>(duration));
+    }
+
+    void set_duration(Duration duration) { _duration = std::move(duration); }
+    const Duration& duration() const noexcept { return _duration; }
+
     ////////////////////
-    void set_time(QTime time) { _time = std::move(time); }
-    const QTime& time() const noexcept { return _time; }
+    void start();
+    void end();
+    void reset() { _started = _ended = never; }
+
+    const Timepoint& started() const noexcept { return _started; }
+    const Timepoint& ended() const noexcept { return _ended; }
+
+    bool is_started() const noexcept { return _started != never; }
+    bool is_ended() const noexcept { return _ended != never; }
+
+    Duration overage() const;
 
 private:
     QString _name;
-    QTime _time { 0, 0, 0 };
+    Duration _duration { 0 };
+
+    Timepoint _started;
+    Timepoint _ended;
+
+    static constexpr Timepoint never { };
+
+    void set_started(Timepoint timepoint) { _started = std::move(timepoint); }
+    void set_ended(Timepoint timepoint) { _ended = std::move(timepoint); }
+
+    friend class SectionReader;
+    friend class SectionWriter;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
