48d154dd22b8d115b6a4e840f225e188a7e50ba2
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-11-10 16:23:50 -0500

    EventWriter: add to_string function

--- src/eventwriter.cpp
+++ src/eventwriter.cpp
@@ -1,7 +1,14 @@
 #include "eventwriter.hpp"
 #include "sectionwriter.hpp"
 
-#include <QXmlStreamWriter>
+#include <QString>
+#include <QDate>
+
+////////////////////////////////////////////////////////////////////////////////
+static inline QString to_string(const QDate& date)
+{
+    return date.toString("MMM d yyyy");
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 void EventWriter::write(QXmlStreamWriter& writer, const Event& event)
@@ -9,10 +16,9 @@ void EventWriter::write(QXmlStreamWriter& writer, const Event& event)
     writer.writeStartElement("event");
 
     writer.writeAttribute("name", event.name());
-    writer.writeAttribute("date", Event::to_string(event.date()));
+    writer.writeAttribute("date", to_string(event.date()));
 
-    for(size_t n = 0; n < event.size(); ++n)
-        SectionWriter::write(writer, event.section(n));
+    for(size_t n = 0; n < event.size(); ++n) SectionWriter::write(writer, event.section(n));
 
     writer.writeEndElement();
 }
