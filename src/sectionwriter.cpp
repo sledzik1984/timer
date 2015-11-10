1f46c0c13b84a89e3bf9b41a2e3be50400ec1140
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-11-10 15:44:14 -0500

    SectionWriter: add to_string functions

--- src/sectionwriter.cpp
+++ src/sectionwriter.cpp
@@ -1,6 +1,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "sectionwriter.hpp"
-#include <QXmlStreamReader>
+
+#include <QDateTime>
+#include <QString>
+#include <QTime>
+
+////////////////////////////////////////////////////////////////////////////////
+static inline QString to_string(const Duration& duration)
+{
+    return QTime(0, 0).addSecs(duration).toString("H:mm:ss");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static inline QString to_string(const QDateTime& datetime)
+{
+    return datetime.toString("MMM d h:mm:ss yyyy");
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 void SectionWriter::write(QXmlStreamWriter& writer, const Section& section)
@@ -11,10 +26,10 @@ void SectionWriter::write(QXmlStreamWriter& writer, const Section& section)
     writer.writeStartElement("section");
 
     writer.writeAttribute("name", section.name());
-    writer.writeAttribute("duration", Section::to_string(section.duration()));
+    writer.writeAttribute("duration", to_string(section.duration()));
 
-    if(section.is_started()) writer.writeAttribute("started", Section::to_string(section.started()));
-    if(section.is_ended()) writer.writeAttribute("ended", Section::to_string(section.ended()));
+    if(section.is_started()) writer.writeAttribute("started", to_string(section.started()));
+    if(section.is_ended()) writer.writeAttribute("ended", to_string(section.ended()));
 
     writer.writeEndElement();
 }
