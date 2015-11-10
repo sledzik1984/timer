b8fbfcdb5838d0ef393d992bb3361e3bb0618f30
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-11-10 00:58:35 -0500

    EventWriter: tweak

--- src/eventwriter.cpp
+++ src/eventwriter.cpp
@@ -1,11 +1,18 @@
 #include "eventwriter.hpp"
 #include "sectionwriter.hpp"
 
+#include <QXmlStreamWriter>
+
 ////////////////////////////////////////////////////////////////////////////////
 void EventWriter::write(QXmlStreamWriter& writer, const Event& event)
 {
     writer.writeStartElement("event");
+
     writer.writeAttribute("name", event.name());
-    for(const Section& section: event.sections()) SectionWriter::write(writer, section);
+    writer.writeAttribute("date", Event::to_string(event.date()));
+
+    for(size_t n = 0; n < event.size(); ++n)
+        SectionWriter::write(writer, event.section(n));
+
     writer.writeEndElement();
 }
