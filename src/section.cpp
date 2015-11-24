dcf4ec39c4ad549681d11036e39f5b288d041123
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-11-24 11:13:46 -0500

    Section: add signals

--- src/section.cpp
+++ src/section.cpp
@@ -7,19 +7,54 @@ Section::Section(QString name, Seconds duration)
 {
     set_name(std::move(name));
     set_duration(std::move(duration));
-    reset();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::swap(Section& s)
+{
+    set_name    (std::move(s._name    ));
+    set_duration(std::move(s._duration));
+    set_started (std::move(s._started ));
+    set_ended   (std::move(s._ended   ));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::set_name(QString name)
+{
+    if(name != _name)
+    {
+        _name = std::move(name);
+        emit name_changed(_name);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::set_duration(Seconds duration)
+{
+    if(duration != _duration)
+    {
+        _duration = std::move(duration);
+        emit duration_changed(_duration);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void Section::start()
 {
-    if(!is_started()) _started = Clock::instance().datetime();
+    if(!is_started()) set_started(Clock::instance().datetime());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void Section::end()
 {
-    if(is_started() && !is_ended()) _ended = Clock::instance().datetime();
+    if(is_started() && !is_ended()) set_ended(Clock::instance().datetime());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::reset()
+{
+    set_ended(QDateTime());
+    set_started(QDateTime());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,7 +62,7 @@ Seconds Section::overage() const
 {
     if(is_started())
     {
-        QDateTime end = started().addSecs(duration());
+        auto end = started().addSecs(duration());
         if(!is_ended())
         {
             int overage = end.secsTo(Clock::instance().datetime());
@@ -37,4 +72,24 @@ Seconds Section::overage() const
     }
 
     return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::set_started(QDateTime started)
+{
+    if(started != _started)
+    {
+        _started = std::move(started);
+        emit started_changed(_started);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void Section::set_ended(QDateTime ended)
+{
+    if(ended != _ended)
+    {
+        _ended = std::move(ended);
+        emit ended_changed(_ended);
+    }
 }
--- src/section.hpp
+++ src/section.hpp
@@ -6,35 +6,37 @@
 #include "clock.hpp"
 
 #include <QDateTime>
+#include <QObject>
 #include <QString>
 
 #include <utility>
 
 ////////////////////////////////////////////////////////////////////////////////
-class Section
+class Section : public QObject
 {
+    Q_OBJECT
+
 public:
     ////////////////////
     Section() = default;
     Section(QString name, Seconds duration);
 
-    Section(const Section&) = delete;
-    Section(Section&&) = default;
+    Section(Section&& s) { swap(s); }
+    Section& operator=(Section&& s) { swap(s); return *this; }
 
-    Section& operator=(const Section&) = delete;
-    Section& operator=(Section&&) = default;
+    void swap(Section& s);
 
     ////////////////////
-    void set_name(QString name) { _name = std::move(name); }
+    void set_name(QString name);
     const QString& name() const noexcept { return _name; }
 
-    void set_duration(Seconds seconds) { _duration = std::move(seconds); }
+    void set_duration(Seconds duration);
     const Seconds& duration() const noexcept { return _duration; }
 
     ////////////////////
     void start();
     void end();
-    void reset() { _started = _ended = QDateTime(); }
+    void reset();
 
     const QDateTime& started() const noexcept { return _started; }
     const QDateTime& ended() const noexcept { return _ended; }
@@ -44,17 +46,25 @@ public:
 
     Seconds overage() const;
 
+signals:
+    ////////////////////
+    void name_changed(const QString&);
+    void duration_changed(const Seconds&);
+
+    void started_changed(const QDateTime&);
+    void ended_changed(const QDateTime&);
+
 private:
     ////////////////////
     QString _name;
-    Seconds _duration { 0 };
+    Seconds _duration = 0;
 
     QDateTime _started;
     QDateTime _ended;
 
     ////////////////////
-    void set_started(QDateTime datetime) { _started = std::move(datetime); }
-    void set_ended(QDateTime datetime) { _ended = std::move(datetime); }
+    void set_started(QDateTime started);
+    void set_ended(QDateTime ended);
 
     friend class SectionReader;
 };
