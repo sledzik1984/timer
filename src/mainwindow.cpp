13b96ab0b291b06efa6ff233efe27fbfcf8eb3b4
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-12-07 21:35:55 -0500

    MainWindow: don't promote widgets

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,13 @@
 ////////////////////////////////////////////////////////////////////////////////
+#include "eventnamewidget.hpp"
 #include "mainwindow.hpp"
+#include "trucktimewidget.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 MainWindow::MainWindow(QWidget* parent) : QWidget(parent)
 {
     setupUi(this);
+
+    panel_0->layout()->addWidget(new EventNameWidget());
+    panel_1->layout()->addWidget(new TruckTimeWidget());
 }
