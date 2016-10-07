618cbf0cf92eb622d03c021de4adc095369bc47e
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2016-10-07 19:09:56 -0400

    VideoTimeWidget: show CountDownWidget
    Connect OscSource events to it.

--- src/videotimewidget.cpp
+++ src/videotimewidget.cpp
@@ -1,4 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
+#include "countdownwidget.hpp"
+#include "oscsource.hpp"
 #include "videotimewidget.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -6,4 +8,13 @@ VideoTimeWidget::VideoTimeWidget(QWidget* parent) :
     StyleWidget(parent)
 {
     setupUi(this);
+
+    auto widget = new CountDownWidget(Qt::gray, Qt::red, QTime(0, 0, 15), this);
+    layout()->addWidget(widget);
+
+    connect(&*OscSource::instance(), &OscSource::time_changed, [widget](const QTime& time, const QTime& total)
+    {
+        widget->set_time(QTime(0, 0, 0).addMSecs(time.msecsTo(total)));
+    });
+    connect(&*OscSource::instance(), &OscSource::status_changed, [widget](Status status) { if(status == Reset) widget->reset(); });
 }
