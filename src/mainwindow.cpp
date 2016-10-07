4ff1c149393d8724e72a394db2f0b2e5c611e682
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2016-10-07 12:23:53 -0400

    MainWindow: show EventTimeWidget and VideoTimeWidget

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////
+#include "eventtimewidget.hpp"
 #include "mainwindow.hpp"
 #include "trucktimewidget.hpp"
+#include "videotimewidget.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 MainWindow::MainWindow(QWidget* parent) : QWidget(parent)
@@ -8,4 +10,6 @@ MainWindow::MainWindow(QWidget* parent) : QWidget(parent)
     setupUi(this);
 
     panel_1->layout()->addWidget(new TruckTimeWidget());
+    panel_2->layout()->addWidget(new EventTimeWidget());
+    panel_3->layout()->addWidget(new VideoTimeWidget());
 }
