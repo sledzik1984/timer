f3dd143b976066eb3ac6439da72f5765209c5cf5
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2016-10-07 17:52:49 -0400

    EventTimeWidget: show CountUpWidget
    Connect OscSource events to it.

--- src/eventtimewidget.cpp
+++ src/eventtimewidget.cpp
@@ -1,9 +1,24 @@
 ////////////////////////////////////////////////////////////////////////////////
+#include "countupwidget.hpp"
 #include "eventtimewidget.hpp"
+#include "oscsource.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 EventTimeWidget::EventTimeWidget(QWidget* parent) :
     StyleWidget(parent)
 {
     setupUi(this);
+
+    auto widget = new CountUpWidget(Qt::gray, this);
+    layout()->addWidget(widget);
+
+    connect(&*OscSource::instance(), &OscSource::status_changed, [widget](Status status)
+    {
+        switch(status)
+        {
+        case Start: widget->start(); break;
+        case Stop : widget->stop() ; break;
+        case Reset: widget->reset(); break;
+        }
+    });
 }
